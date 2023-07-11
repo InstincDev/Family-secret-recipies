1d99cfe68ea54f754390fbd0f2caf2ef72af8440
InstincDev <crene7722@gmail.com>
2023-07-11 16:11:34 -0500

    render recipeCard for specified recipePin on recipeSlide

--- client/src/components/RecipeCard/RecipeCard.jsx
+++ client/src/components/RecipeCard/RecipeCard.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import {
     title,
@@ -8,38 +9,53 @@ import {
     comments,
 } from "./RecipeCard.module.sass";
 import NavBar from "../../components/NavBar/NavBar";
-import { useEffect, useState } from "react";
+import { RecipeAPIContext } from "../../utils/RecipeAPIContext.jsx"
 
-const RecipeCard = ({ recipeList }) => {
+const RecipeCard = () => {
     const { id } = useParams();
-    const [recipe, setRecipe] = useState([]);
+    const {recipeData} = useContext(RecipeAPIContext)
+    const recipe = recipeData.find((recipe) => recipe._id === id)
+ 
+    console.log(id);
+    
+//  useEffect(() => {
 
-    useEffect(() => {
-        setRecipe(recipeList.filter((recipes) => recipes._id === id));
-    }, [recipeList]);
-    console.log(recipe);
-    return (
+//       const showRecipe= async()=>{
+//         try {
+//              const card = recipeData.map(meals =>{
+//                 meals.filter((recipes) => console.log(recipes._id === id))
+//              })
+//                     setRecipe(card)
+//         } catch (error) {
+//             console.error(error.message)
+//         }
+       
+    
+//     }
+// }, [recipeData, setRecipe]);
+   console.log(recipe);
+    
+   return (
         <>
             <NavBar />
-            {recipe.map((recipes, i) => (
-                <div key={i}>
-                    <h1>{recipes.meal}</h1>
+                <div>
+                    <h1>{recipe.meal}</h1>
                     
                     <div className={recipeCard}>
                         <section class={title}>
-                            <h3>Created By: {recipes.createdBy}</h3>
+                            <h3>Created By: {recipe.createdBy}</h3>
                             <div>
-                                <span>{recipes.category}</span> - <span>{recipes.area}</span>
+                                <span>{recipe.category}</span> - <span>{recipe.area}</span>
                             </div>
                             
                         </section>
                         <aside class={recipeInfo}>
-                            <img src={recipes.image} alt="Placeholder Image" />
+                            <img src={recipe.image} alt="Placeholder Image" />
 
                             <section class={ingredients}>
                                 <h3>Ingredients</h3>
                                 <ul>
-                                    {recipes.ingredient.map((inge, i) => (
+                                    {recipe.ingredient.map((inge, i) => (
                                         <li
                                             key={`inge${i}`}
                                         ><span>{inge.ingredient}</span> - <span>{inge.measurement}</span></li>
@@ -47,12 +63,12 @@ const RecipeCard = ({ recipeList }) => {
                                 </ul>
                             </section>
                         </aside>
-                        <small>tags: <ul>{recipes.tag.map((tag,i)=>(<li key={`tag${i}`}>{tag}</li>))}</ul></small>
+                        <small>Tags: {recipe.tag && <ul>{recipe.tag.map((tags, i) => (<li key={`tag${i}`}>{tags}</li>))}</ul>}</small>
                     </div>
                     <div className={directions}>
                         <section>
                             <h3>Directions</h3>
-                            <p>{recipes.instructions}</p>
+                            <p>{recipe.instructions}</p>
                         </section>
 
                         <section class={comments}>
@@ -91,7 +107,7 @@ const RecipeCard = ({ recipeList }) => {
                         </section>
                     </div>
                 </div>
-            ))}
+            
         </>
     );
 };
--- client/src/components/Sliders/RecipeSlide.jsx
+++ client/src/components/Sliders/RecipeSlide.jsx
@@ -6,12 +6,6 @@ import { fetchRecipes } from "../../utils/serverRequests.js"
 import RecipePin from "../RecipePin/RecipePin";
 import { wrapper, mealList } from "../Sliders/Sliders.module.sass";
 
-
-//ToDo
-// create useEffect fn to get random recipes by passing state obj array
-// map separate array elems into sliders
-// Add subTitle for each slider
-
 const RecipeSlide = ({  title, slideList }) => {
     const {recipeData} = useContext(RecipeAPIContext)
     const [recipeSlide, setRecipeSlide] = useState([]);
