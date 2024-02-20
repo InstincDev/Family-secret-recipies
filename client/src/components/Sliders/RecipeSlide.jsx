978cc5832f3f60a7e70893142efa5779b1ec47e4
ChanelT <67307808+InstincDev@users.noreply.github.com>
2024-02-19 16:31:41 -0800

    update favorites (#21)
    * Fix home pg render (#19)
    
    * add sample recipe page and begin updating css
    
    * add ingredients, directions, and comments to sample recipe page.
    
    * update recipe card layout
    
    * setup vite environment
    
    * create header component and project css
    
    * removed node_modules folder
    
    * install node and mongo
    
    * create express server
    
    * add routes to HomePage RecipePage and UserProfilePage
    
    * create ingredientSchema and updated mealSchema
    
    * created post and get routes for seedDB
    
    * removed comments and cleaned up code
    
    * created new Meal documents to seed database
    
    * removed unnessecerycomments
    
    * made GET request to backend server
    
    * seeded database with recipes
    
    * added useEffect and started styling content
    
    * add recipeCard template to react format and reconfig files to export default
    
    * updated app.js imports and recipeCard styling
    
    * fix back buttons to and from homePage and recipePage
    
    * add useParams to and refactor links to dynamically pages
    
    * add vite env vars to project
    
    * created sliders using react splide
    
    * created sliders for Sliders components, updated imports to HomePage, lifted sass file to cover all sliders, planned display of slider recipes
    
    * created state for area and category types
    
    * created single fn to get slider types, saved slider types to slider state, passed slider state to HomePage
    
    * separated sliderTypes into specific state hooks, passed state to correct slider component, updated fn to fetch recipes by group and type
    
    * updated fetchrecipeSlides fn, refactored getRecipes fn in Areas component, Areas component html displays specific areas rescipes
    
    * removed unnecessary comments in HomePage
    
    * updated Category component to display random recipes according to a random category type
    
    * created RecipeSlide component to replace all specific component slides
    
    * refactored RecipeSlide component to map over slideList of types for each recipe slide
    
    * began major refactor, added useContext to pass API data throughout the app
    
    * all sections render random recipes for each slider type
    
    * merged working recipeSlides branch
    
    * finished merging random slides
    
    * render recipeCard for specified recipePin on recipeSlide (#17)
    
    * reset homepage layout to display set num of random slide types
    
    * added a back link to RecipeCard file
    
    * added Login component and LoginPage page, started setting up passport auth and cookieSessions
    
    * Moved NavBar component to  App.jsx
    
    * added passport google strategy to backend
    
    * added google passport strategy and sessions
    
    * added useLocalStorage
    
    * temp fixed HomePage render and added MVP todos
    
    * fixed recipeCard errors
    
    * removed additional NavBar component
    
    * Show all slides (#20)
    
    * reviewed recipeSlide component, added useState hook for showAll function, added showAll button and onClick function
    
    * fixed bug with state

--- client/src/components/Sliders/RecipeSlide.jsx
+++ client/src/components/Sliders/RecipeSlide.jsx
@@ -3,108 +3,142 @@ import { RecipeAPIContext } from "../../utils/RecipeAPIContext.jsx";
 import useLocalStorage from "../../utils/useLocalStorage.jsx";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
-import { fetchRecipes } from "../../utils/serverRequests.js"
+import { fetchRecipes } from "../../utils/serverRequests.js";
 import RecipePin from "../RecipePin/RecipePin";
 import { wrapper, mealList } from "../Sliders/Sliders.module.sass";
 
-const RecipeSlide = ({  title, slideList }) => {
-    const {recipeData} = useContext(RecipeAPIContext)
-    const [recipeSlide, setRecipeSlide] = useLocalStorage("recipeSlide"+title,[])
 
-     useEffect(() => {
-        const getRecipes = async () => {
-            try {
-                const slides = slideList.map((slide)=>{
-                    const recipes = recipeData.filter((recipe)=>{
-                       
-                        if(title === "ingredient"){
-                        for (const ingre of recipe[title]) {
-                            if(ingre[title] === slide){
-                                return ingre[title] === slide
-                            }   
-                        }
-                       }else if(title === "tag"){
-                        
-                        for (const tags in recipe[title]) {
-                            return recipe[title][tags] === slide
-                        }
-                       } else{ return recipe[title]  === slide}
-                    })
-                    
-                    
-                    const randomRecipes = getRandomTypes(recipes, 10)
-                    return randomRecipes
-                })
-                if(recipeSlide.length == 0){setRecipeSlide(slides);}
-                
-            } catch (error) {
-                console.error(error.message);
-            }
-        };
+const RecipeSlide = ({ title, slideList }) => {
+  const { recipeData } = useContext(RecipeAPIContext);
+  const [showAll, setShowAll] = useState(false);
+  const [recipeSlide, setRecipeSlide] = useLocalStorage(
+      "recipeSlide" + title,
+      []
+  );
 
-        getRecipes();
-    }, [slideList, title,recipeData]);
+  useEffect(() => {
+      const getRecipes = async () => {
+          try {
+              const slides = slideList.map((slide) => {
+                  const recipes = recipeData.filter((recipe) => {
+                      if (title === "ingredient") {
+                          for (const ingre of recipe[title]) {
+                              if (ingre[title] === slide) {
+                                  return ingre[title] === slide;
+                              }
+                          }
+                      } else if (title === "tag") {
+                          for (const tags in recipe[title]) {
+                              return recipe[title][tags] === slide;
+                          }
+                      } else {
+                          return recipe[title] === slide;
+                      }
+                  });
 
-    // console.log(recipeSlide);
-    // console.log(recipeData);
-    
-    function getRandomTypes(typeArray, count) {
-        const randomIndices = numberSet(count, typeArray.length);
-        const randomTypes = [];
+                  console.log(recipes.length);
 
-        for (const index of randomIndices) {
-            randomTypes.push(typeArray[index]);
-        }
+                  const shownRecipes = !showAll
+                      ? getRandomTypes(recipes, recipes.length)
+                      : recipes;
 
-        return randomTypes;
-    }
+                  return shownRecipes;
+              });
+              if (recipeSlide.length == 0) {
+                  setRecipeSlide(slides);
+              }
+          } catch (error) {
+              console.error(error.message);
+          }
+      };
 
-    function numberSet(desired, max) {
-        // condition makes sure while loop isn't sticky
-        if (max < desired) {
-            desired = max;
-        }
+      getRecipes();
+  }, [slideList, title, recipeData]);
 
-        const set = new Set();
+  // console.log(recipeSlide);
+  // console.log(recipeData);
 
-        while (set.size < desired) {
-            set.add(Math.floor(Math.random() * max));
-        }
+  function getRandomTypes(typeArray, count) {
+      const randomIndices = numberSet(count, typeArray.length);
+      const randomTypes = [];
 
-        return [...set];
-    }
-    
-    return (
-        
-        <div>
+      for (const index of randomIndices) {
+          randomTypes.push(typeArray[index]);
+      }
+
+      return randomTypes;
+  }
+
+  function numberSet(desired, max) {
+      // condition makes sure while loop isn't sticky
+      if (max < desired) {
+          desired = max;
+      }
+
+      const set = new Set();
+
+      while (set.size < desired) {
+          set.add(Math.floor(Math.random() * max));
+      }
+
+      return [...set];
+  }
+  function handleShowAll() {
+      setShowAll(!showAll);
+  }
+  return (
+      <div>
           {slideList.map((slide, index) => (
-            <div key={index}>
-              <h3>{slide}</h3>
-              <div>
-                <Splide options={{ perPage: 4, pagination: false, drag: 'free' }}>
-                  {recipeSlide[index] &&
-                    recipeSlide[index].map((recipe, i) => (
-                      <SplideSlide key={`recipeList-${i}`}>
-                        <div className={mealList}>
-                          <RecipePin
-                            key={`recipeList-${slide}-${i}`}
-                            id={recipe._id}
-                            meal={recipe.meal}
-                            description={recipe.category}
-                            image={recipe.image}
-                          />
-                        </div>
-                      </SplideSlide>
-                    ))}
-                </Splide>
-              </div>
-            </div>
-          ))}
-        </div>
-      );
-    
+              <div key={index}>
+                  <h3>{slide}</h3>
+                  <div>
+                      <Splide
+                          options={{
+                              perPage: 4,
+                              pagination: false,
+                              drag: "free",
+                          }}
+                      >
+                          {recipeSlide[index] &&
+                              recipeSlide[index].map((recipe, i) => (
+                                  <SplideSlide
+                                      key={`recipeList-${i}`}
+                                      
+                                  >
+                                      <div className={mealList}>
+                                          {console.log(recipeSlide[index].length)}
+                                         
+                                         {i <= 10 && !showAll &&
+                                          <RecipePin
+                                              key={`recipeList-${slide}-${i}`}
+                                              id={recipe._id}
+                                              meal={recipe.meal}
+                                              description={recipe.category}
+                                              image={recipe.image}
+                                          />}
+                                            {i === 10 && <button
+                                                      onClick={handleShowAll}>
+                                                      Show All
+                                                  </button>}
 
-   
+                                            {showAll && 
+                                            <RecipePin
+                                              key={`recipeList-${slide}-${i}`}
+                                              id={recipe._id}
+                                              meal={recipe.meal}
+                                              description={recipe.category}
+                                              image={recipe.image}
+                                          />}
+                                      </div>
+                                      
+                                  </SplideSlide>
+                              ))}
+                      </Splide>
+                  </div>
+              </div>
+          ))}
+      </div>
+  );
 };
 
 export default RecipeSlide;
